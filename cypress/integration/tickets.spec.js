733598dcc72ed583a08b2dfb2709b2bb005dc492
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-08-02 22:38:21 -0300

    Navegando na URL e preenchendo textos

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,7 +1,15 @@
-const cypress = require("cypress");
-const { it } = require("mocha");
-
 describe("Tickets",()=>{
     beforeEach(()=>cy.visit("https://bit.ly/2XSuwCW"));
+
+    it.only("fills all the text input fields", ()=>{
+        const firstName = "Yuri";
+        const lastName = "Test Cypress";
+
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#email").type("teste1@gmail.com");
+        cy.get("#requests").type("Vegetarin");
+        cy.get("#signature").type(`${firstName} ${lastName}`);
+    })
     it("has 'TICKETBOX' header's heading" , () => {});
 });
